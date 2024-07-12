a4c5c6aff87069c8dba880edf474323067befa77
mdwasifulkabir <wasifulkabir9016@gmail.com>
2024-07-12 21:42:02 +0800

    Change key layout

--- script.js
+++ script.js
@@ -41,36 +41,30 @@ function operate(num1, num2, operator) {
   return result;
 }
 
-const numberContainer = document.querySelector("#number-container");
-const operatorContainer = document.querySelector("#operator-container");
+const buttonContainer = document.querySelector("#button-container");
 const display = document.querySelector("#display");
 
-numberContainer.addEventListener("click", (e) => {
-  if (!e.target.classList.contains("numbers")) return;
+buttonContainer.addEventListener("click", (e) => {
+  if (!e.target.classList.contains("numbers") && !e.target.classList.contains("operators")) return;
   if (display.textContent === "Nope") display.textContent = "";
   
   const keyText = e.target.textContent;
-  if (keyText === '.'){
-    //check if there is an operator
-    const opIndex = display.textContent.split('').findIndex(c => operators.includes(c));
-    /*if there's no operator, dont let the user enter another decimal if one 
-    exists on the screen */
-    if (opIndex === -1) {
-      if (display.textContent.includes('.')) return;
-    //if there is an operator, extract the second number and check if it contains a decimal
-    } else {
-      const num2 = display.textContent.slice(opIndex);
-      if(num2.includes('.')) return;
-    }
-  }
-  display.textContent += keyText;
-});
 
-operatorContainer.addEventListener("click", (e) => {
-  const keyText = e.target.textContent;
-  if (display.textContent === "Nope") display.textContent = "";
-  
-  switch(keyText) {
+  switch (keyText){
+    case '.':
+      //check if there is an operator
+      const opIndex = display.textContent.split('').findIndex(c => operators.includes(c));
+      /*if there's no operator, dont let the user enter another decimal if one 
+      exists on the screen */
+      if (opIndex === -1) {
+        if (display.textContent.includes('.')) return;
+      //if there is an operator, extract the second number and check if it contains a decimal
+      } else {
+        const num2 = display.textContent.slice(opIndex);
+        if(num2.includes('.')) return;
+      }
+      break;
+
     case "AC":
       display.textContent = "";
       break;
@@ -80,16 +74,21 @@ operatorContainer.addEventListener("click", (e) => {
       break;
 
     default:
-      //check if display already has a operator and if it does calculate that first
-      const hasOperator = display.textContent.split('').find(c => operators.includes(c));
-      //check if the last character is an operator
-      const isLastOp = operators.includes(display.textContent.at(-1));
+      if (e.target.classList.contains("numbers")) {
+        display.textContent += keyText;
+      }
+      else if (e.target.classList.contains("operators")) {
+        //check if display already has a operator and if it does calculate that first
+        const hasOperator = display.textContent.split('').find(c => operators.includes(c));
+        //check if the last character is an operator
+        const isLastOp = operators.includes(display.textContent.at(-1));
 
-      if(hasOperator && !isLastOp) {
-        calculate(display.textContent);
-        display.textContent += keyText;
-      } else if(display.textContent != "" && !isLastOp) {
-        display.textContent += keyText;
+        if(hasOperator && !isLastOp) {
+          calculate(display.textContent);
+          display.textContent += keyText;
+        } else if(display.textContent != "" && !isLastOp) {
+          display.textContent += keyText;
+        }
       }
   }
 });
